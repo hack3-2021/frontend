24411c24a425db1e01d6a243821d57cc10ceb0d9
= <=>
2021-06-27 16:23:33 +1000

    small change to js for git testing

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,6 @@ function render_post(profile_img, name, contents) {
     $('#stream').prepend(`<div class="post"><img class="${profile_img}" src="img_src"><b><p class="name">${name}</p></b><p class="body">${contents}</p></div>`);
 }
 
-
-
+function fetch_user(username) {
+    console.log("");   
+}
