17f978df454d4a229d6b64ecc031106df7921a32
= <=>
2021-06-27 18:44:42 +1000

    spacer fix

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,8 @@ let test_community = "bankstown"
 function render_post(post) {
     let $container = $('#stream');
     $container.prepend(`<div class="spacer"></div><div class="post"><img class="profile_picture"><b><p class="post_username"></p></b><p class="post_body"></p></div>`);
-    let $post = $container.children(":first");
+    let $post = $container.children().eq(1);
+    console.log(post.html());
     $post.find("img").attr("src", post["poster"]["picture"]);
     $post.find(".post_username").text( post["poster"]["firstName"] + " " + post["poster"]["lastName"]);
     $post.find(".post_body").text(post["msg"]);
