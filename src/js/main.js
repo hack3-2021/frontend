9778d298b5b77d77eb1693cf50db692cdeda1436
= <=>
2021-06-27 18:11:42 +1000

    ignore

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@ let test_email = "normal.human@gmail.com"
 async function render_post(email, contents) {
     let $container = $('#stream');
     $container.prepend(`<div class="post"><img class="profile_picture"><b><p class="post_username"></p></b><p class="post_body"></p></div>`);
-    let $post = $container.children("div.first");
+    let $post = $container.children(":first");
 
     $.ajax({
         url: "/api/profile?email=" + email,
