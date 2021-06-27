fa0372f6b983404815efc5b84f6fc07194bfb06a
= <=>
2021-06-27 17:16:58 +1000

    testing async post rendering

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,26 @@
-function render_post(profile_img, name, contents) {
-    $('#stream').prepend(`<div class="post"><img class="${profile_img}" src="img_src"><b><p class="name">${name}</p></b><p class="body">${contents}</p></div>`);
+async function render_post(email, contents) {
+
+    let $container = $('#stream');
+    $container.prepend(`<div class="post"><img><b><p class="name"></p></b><p class="body"></p></div>`);
+    
+    let promise = new Promise( (resolve, reject) => { resolve(fetch_user(email)) } );
+    let result = await promise;
+    
+    console.log(result)
+    //$container.children("img, p").click();
 }
 
-function fetch_user(username) {
-    console.log("");   
+function fetch_user(email) {
+    let user_data = {};
+    $.ajax({
+        url: "/api/profile?email=" + email,
+        type: 'GET',
+        dataType: 'json',
+        "async": true,
+    }).done( function (response) {
+        console.log(response)
+        user_data = response
+    })
+    return user_data;
 }
+
