cab9d75542196a462165b95acd0c57786ab9e16c
= <=>
2021-06-27 18:47:13 +1000

    b

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,7 @@ function render_post(post) {
     let $container = $('#stream');
     $container.prepend(`<div class="spacer"></div><div class="post"><img class="profile_picture"><b><p class="post_username"></p></b><p class="post_body"></p></div>`);
     let $post = $container.children(":first").next();
-    console.log(post.html());
+    console.log($post.html());
     $post.find("img").attr("src", post["poster"]["picture"]);
     $post.find(".post_username").text( post["poster"]["firstName"] + " " + post["poster"]["lastName"]);
     $post.find(".post_body").text(post["msg"]);
